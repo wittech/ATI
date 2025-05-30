2f8c8fd4fccf47124e39691f03611a50020a8ba4
Angtian Wang <63261150+Angtian@users.noreply.github.com>
2025-05-29 21:02:04 -0700

    Update __init__.py

--- wan/__init__.py
+++ wan/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (c) 2024-2025 Bytedance Ltd. and/or its affiliates
+# SPDX-License-Identifier: Apache-2.0
+
 from . import configs, distributed, modules
 from .first_last_frame2video import WanFLF2V
 from .image2video import WanATI
